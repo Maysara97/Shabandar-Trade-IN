ad0d0914d4c3ebf6fe116846eb19545a21b3bd6d
Christin Magdy <cmagdy@bebrand.tv>
2020-05-15 20:42:47 +0200

    Update Environment

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://api.youtradein.com/api',
-    file_path: 'https://api.youtradein.com/files/',
     apiDomain: 'api.youtradein.com',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'https://localhost:5001/api',
+    baseUrl: 'https://api.youtradein.com/api',
+    file_path: 'https://api.youtradein.com/files/',
+    apiDomain: 'api.youtradein.com',
 }
 
 /*
