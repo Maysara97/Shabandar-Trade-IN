593cd3e4a00322eab97e3125f4057765a34fee05
Christin Magdy <cmagdy@bebrand.tv>
2020-08-26 11:29:11 +0200

    Landing Page design finished

--- src/app/home/home-layout/home-footer/home-footer.component.ts
+++ src/app/home/home-layout/home-footer/home-footer.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit, HostBinding } from "@angular/core";
+import { Component, OnInit, HostBinding } from '@angular/core'
 
 @Component({
-  selector: "app-home-footer",
-  templateUrl: "./home-footer.component.html",
-  styleUrls: ["./home-footer.component.scss"]
+    selector: 'app-home-footer',
+    templateUrl: './home-footer.component.html',
+    styleUrls: ['./home-footer.component.scss'],
 })
 export class HomeFooterComponent implements OnInit {
-  myBackgroundImageUrl = "assets/images/You-Trade-In/background.png";
-  constructor() {}
+    myBackgroundImageUrl = 'assets/images/You-Trade-In/background.png'
+    constructor() {}
 
-  ngOnInit() {}
-  @HostBinding("style.backgroundImage")
-  getBackgroundImageUrl() {
-    return `url(${this.myBackgroundImageUrl})`;
-  }
+    ngOnInit() {}
+    @HostBinding('style.backgroundImage')
+    getBackgroundImageUrl() {
+        return `url(${this.myBackgroundImageUrl})`
+    }
 
-  public navigateToSection(section: string) {
-    window.location.hash = "";
-    window.location.hash = section;
-  }
+    public navigateToSection(section: string) {
+        window.location.hash = ''
+        window.location.hash = section
+    }
 }
--- src/app/home/home-main/signup/signup.component.ts
+++ src/app/home/home-main/signup/signup.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, HostBinding } from "@angular/core";
+import { Component, OnInit, HostBinding } from '@angular/core'
 
 @Component({
-  selector: "app-signup",
-  templateUrl: "./signup.component.html",
-  styleUrls: ["./signup.component.scss"]
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  myBackgroundImageUrl = "assets/images/You-Trade-In/SignUp2.png";
-  constructor() {}
+    myBackgroundImageUrl = 'assets/images/You-Trade-In/SignUp2.png'
+    constructor() {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  @HostBinding("style.backgroundImage")
-  getBackgroundImageUrl() {
-    return `url(${this.myBackgroundImageUrl})`;
-  }
+    @HostBinding('style.backgroundImage')
+    getBackgroundImageUrl() {
+        return `url(${this.myBackgroundImageUrl})`
+    }
 }
