6aeff2103d0957b007d36e0dc457bc18362d2823
Maysara-Nasser <mmamdouh@bebrand.tv>
2020-11-25 10:08:17 +0200

    shareing updates

--- src/app/application/components/product-details/product-details.component.ts
+++ src/app/application/components/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, TemplateRef } from '@angular/core'
+import { BuyingProductDetailsComponent } from './../buying-product-details/buying-product-details.component';
+import { Component, OnInit, TemplateRef ,OnDestroy } from '@angular/core'
 import { AccountProductService } from '../../services/accountProduct.service'
 import { AccountProduct } from '../../models/accountProduct'
 import { ActivatedRoute } from '@angular/router'
@@ -11,8 +12,13 @@ import {
     FinishedStatusTypeMapping,
 } from '../../models/enum'
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal'
+<<<<<<< HEAD
 import { AccountData } from 'src/app/account/models/register'
 import { AuthService } from 'src/app/shared/services/auth.service'
+=======
+// import { Page } from 'ngx-pagination/dist/pagination-controls.directive';
+// import { Page } from 'tns-core-modules/ui/page';
+>>>>>>> shareing updates
 
 
 @Component({
@@ -43,7 +49,8 @@ export class ProductDetailsComponent implements OnInit {
         private modalService: BsModalService,
         private auth: AuthService,
         private meta: Meta,
-        private title: Title
+        private title: Title,
+        
     ) {
         this.accountProductId = route.snapshot.params['accountProductId']
         this.env = environment
@@ -52,8 +59,16 @@ export class ProductDetailsComponent implements OnInit {
     }
 
     ngOnInit(): void {
-            
-        // var imgSrc = $("#productImg").attr("src");
+       
+        //Removing The Website Meta Tags
+        this.meta.removeTag("name='og_url'")
+        this.meta.removeTag("name='og_image'")
+        this.meta.removeTag("name='og_type'")
+        this.meta.removeTag("name='og_image_width'")
+        this.meta.removeTag("name='og_image_height'")
+        this.meta.removeTag("name='og_description'")
+        this.meta.removeTag("name='og_title'")
+        this.meta.removeTag("name='description'")
         
         this.accountProductService
             .getAccountProductById(this.accountProductId)
@@ -64,21 +79,21 @@ export class ProductDetailsComponent implements OnInit {
                 this.whatsappPost =  `https://api.whatsapp.com/send?text=${'\n' +encodeURI(this.accountProductDetails.productName + '\n' + this.accountProductDetails.description + '\n')} ${this.postUrl}`;
                 this.twitterPost =  `https://twitter.com/share?url=${this.postUrl}&text=${'\n' +encodeURI(this.accountProductDetails.productName + '\n' + this.accountProductDetails.description + '\n' )}`;
                 this.imgUrl = this.getFilePath(this.accountProductDetails.productImages[0]);
-
                 this.title.setTitle(this.accountProductDetails.productName)
+                
+                //Adding Meta Tags For Specific Product
                 this.meta.addTags([
-                    { name: 'keywords', content: '' },
-                    { name: 'Title', content: this.accountProductDetails.productName },
-                    { name: 'author', content: 'Digamber Singh' },
-                    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                    { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
-                  ]);
 
-                this.meta.updateTag({name:'og_image', property:'og:image', Content:this.imgUrl})
-                this.meta.updateTag({name:'og_description' ,property:'og:description' , content:this.accountProductDetails.description})
-                this.meta.updateTag({name:'description' , content:this.accountProductDetails.description})
-                this.meta.updateTag({name:'og_url' , property:'og:url' , content:this.postUrl})
-                this.meta.updateTag({name:'og_title', property:'og:title', content:this.accountProductDetails.productName})
+                    { name:"og_url" , property:"og:url" , content:this.postUrl},
+                    { name:"og_type" , property:"og:type" , content:"website"},
+                    { name:"og_image", property:"og:image", Content:this.imgUrl},
+                    { name:"og_image_width", property:"og:image:width", content:"600"},
+                    { name:"og_image_height", property:"og:image:height", content:"600"},
+                    { name:"og_description", property:"og:description", content:this.accountProductDetails.description},
+                    { name:"og_title", property:"og:title", content:this.accountProductDetails.productName},
+                    { name: 'Title', content: this.accountProductDetails.productName},
+                    { name:"description", content: this.accountProductDetails.description}
+                  ]);
 
             });
 
@@ -104,4 +119,36 @@ export class ProductDetailsComponent implements OnInit {
     closeSubDialog(): void {
         this.viewImageModal.hide()
     }
+
+    ngOnDestroy() :void{
+         //Restore The Title When Navigate Away
+        this.title.setTitle("Shah-bandar Trade")
+        
+        //Removing The Product Meta Tags
+        this.meta.removeTag("name='og_url'")
+        this.meta.removeTag("name='og_image'")
+        this.meta.removeTag("name='og_type'")
+        this.meta.removeTag("name='og_image_width'")
+        this.meta.removeTag("name='og_image_height'")
+        this.meta.removeTag("name='og_description'")
+        this.meta.removeTag("name='og_title'")
+        this.meta.removeTag("name='description'")
+        this.meta.removeTag("name='Title'")
+
+
+        //Adding Meta Tags For The Website
+        this.meta.addTags([
+            { name: 'og_title', content: "Shah-bandar Trade"},
+            { name:"og_url", property:"og:url", content:"https://shahbandartrade.com/home/home-main"},
+            { name:"og_type" ,property:"og:type", content:"website" },
+            { name:"og_image", property:"og:image", content:"https://i.ibb.co/6wNTHyB/logo.png"},
+            { name:"og_image_width", property:"og:image:width", content:"600" },
+            { name:"og_image_height", property:"og:image:height", content:"600"},
+            { name:"og_description", property:"og:description", content:"Shahbandar is a digital platform that will have forms to be filled for merchants."},
+            { name:"description", content: "Shahbandar is a digital platform that will have forms to be filled for merchants."}
+            
+          ]);
+        
+    }
+
 }
